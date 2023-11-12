3e0efb6e2516e60c4c07f8cc40bf0c7390f2e278
clu23 <clement.lulurga@orange.fr>
2023-11-12 16:02:45 +0100

    completing about js file

--- src/about.js
+++ src/about.js
@@ -2,7 +2,29 @@ import { $content, createHtmlElement } from "./index";
 
 
 function render(){
+    const $main = document.createElement("main");
+    $main.classList.add("main");
 
+    const $welcome = createHtmlElement(
+        "p",
+        null,
+        ["cursive"],
+        "Welcome to"
+      );
+    const $title = createHtmlElement("h1", null, ["gray"], "Meat House");
+    
+    const $hr = document.createElement("hr");
+
+    const content =
+    "We are a modern restaurant dedicated to meat in all its forms. Our desire is to let our customers discover all the different ways meat can be prepared and serve with various sauces and accompaniment";
+    const $p = createHtmlElement("p", null, ["menu-item"], content);
+
+    $main.appendChild($welcome);
+    $main.appendChild($title);
+    $main.appendChild($hr);
+    $main.appendChild($p);
+
+    $content.appendChild($main);
 }
 
 export { render as renderAbout}
